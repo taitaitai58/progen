4297a10cc7a9724a2ee9915df45604410bd6d916
kawase748 <y6797258@gmail.com>
2025-12-08 15:56:22 +0900

    tesuto

--- start.py
+++ start.py
@@ -5,11 +5,12 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("START画面")
 
+font = pygame.font.SysFont("MS ゴシック",80)
+
 running = True
 while running:
     screen.fill((0,0,75))
 
-    font = pygame.font.SysFont(None,80)
     message = font.render("START(sキーをクリック)", False, (255,255,255))
     screen.blit(message, (200,300))
 
@@ -19,7 +20,7 @@ while running:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s:
-                subprocess.Popen(["python", "move.py"])  # ←move.py起動
+                subprocess.Popen(["python", "test_game.py"])  # test_game.py起動
                 running = False  # START画面を終了（これがあると「完全に切り替え」になる）
 
     pygame.display.update()
